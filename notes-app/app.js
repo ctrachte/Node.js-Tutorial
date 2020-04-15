41d1e97d832a5bd9f9c462eb1942cb42d2dccc62
ctrachte <cetrachte@hotmail.com>
2020-04-15 13:27:27 -0500

    completed challenge!

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,12 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'My name is Caleb Trachte');
+fs.writeFileSync('notes.txt', 'My name is Caleb Trachte. ');
+
+// Challenge Code:
+try {
+    fs.appendFileSync('notes.txt', 'We appended this text!');
+    console.log('The "text to append" was appended to file!');
+  } catch (err) {
+    /* Handle the error */
+    console.log('The "text to append" was not appended to file!' + err.text);
+  }
