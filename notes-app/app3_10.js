c7a5a539d844703cadad9c5caa670f9dd3f2207d
ctrachte <cetrachte@hotmail.com>
2020-04-15 13:46:07 -0500

    add challenge code for lesson 3.10

--- notes-app/app3_10.js
+++ notes-app/app3_10.js
@@ -1,7 +1,8 @@
 const add = require('./utils3_10.js')
-
+const getNotes = require('./notes3_10.js')
 // const name = 'Caleb Trachte'
 
 const sum = add(2,2)
 // console.log(add)
-console.log(sum)
+// console.log(sum)
+console.log(getNotes)
