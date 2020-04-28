ee8492bfcf268951ee8c7bab85e567afaf844596
ctrachte <cetrachte@hotmail.com>
2020-04-28 10:43:40 -0500

    add logic to check for duplicates, handle them

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -8,13 +8,19 @@ const getNotes = function () {
 // Lesson 4.19
 const addNote = (title, body) => {
     const notes = loadNotes();
-
-    notes.push({
-        title: title,
-        body: body
+    const duplicateNotes = notes.filter((note)=> {
+        return note.title === title;
     });
-
-    saveNotes(notes);
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        });
+        saveNotes(notes);
+        console.log('New note added!');
+    } else {
+        console.log('Note title "' + title + '" is already taken!');
+    }
 };
 
 const saveNotes = (notes) => {
